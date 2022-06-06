ba042f3852142f155b13de7004d629c214ae3b0f
rupamShaw1998 <rupamshw77777@gmail.com>
2022-06-06 22:38:05 +0530

    sorting & filtering

--- Ecommerce/Backend/src/controllers/entity.controllers.js
+++ Ecommerce/Backend/src/controllers/entity.controllers.js
@@ -8,9 +8,22 @@ router.get('/', async(req, res) => {
         const page = req.query.page || 1;
         const size = req.query.size || 7;
         const skip = (page-1)*size;
-        const sorting = req.query.sort()
-        const product = await Product.find().skip(skip).limit(size).sort().lean().exec();
-        const totalPages = Math.ceil((await Product.find().countDocuments())/size);
+        let sorting = req.query.sort || 1;
+        // let brand = req.query.brand;
+        const productBrand = req.query.productBrand;
+        let filter = {};
+
+        if(productBrand) {
+            filter.productBrand = { $in: productBrand }
+        }
+        
+        if(sorting==1) 
+            sort = {priceOfProduct: 1}
+        else
+            sort = {priceOfProduct: -1}
+
+        const product = await Product.find(filter).skip(skip).limit(size).sort(sort).lean().exec();
+        const totalPages = Math.ceil((await Product.find(filter).countDocuments())/size);
         return res.status(200).send({product, totalPages});
     }
     catch(err) {
